f2757c589d5cc2d94f21e642cd96a2913311cb00
csilva <jarskewow@hotmail.com>
2026-04-23 17:41:44 +0100

    v6

--- drone.py
+++ drone.py
@@ -20,8 +20,9 @@ class Drone():
         """Return the immediate next hub in the path"""
         return self.path[-1] if self.path else None
 
-    def walk(self) -> None:
+    def walk(self) -> str | None:
         """Advance the drone by one simulation turn"""
+        # arrived at destination
         if self.in_transit:
             dest = self.destination
             edge = self.transit_edge
@@ -32,24 +33,34 @@ class Drone():
             self.in_transit = False
             self.transit_edge = None
             self.destination = None
+            return f"{self.name}-{self.vertex.name}"
 
         if not self.has_path():
             return None
 
-        used_edge = self.vertex.get_edge(self.path[-1])
-        if self.path[-1].is_possible() and used_edge.is_available():
+        target = self.path[-1]
+        used_edge = self.vertex.get_edge(target)
+
+        # normal zones and priority
+        if target.metadata["zone"] != "restricted" and target.is_possible() and used_edge.is_available():
             self.vertex.drones.remove(self)
             used_edge.att_n_drones()
-            self.path[-1].drones.append(self)
-            self.vertex = self.path[-1]
+            target.drones.append(self)
+            self.vertex = target
             self.path.pop()
             return f"{self.name}-{self.vertex.name}"
 
-        if not self.path[-1].is_possible():
+        # restricted zones
+        if target.metadata["zone"] == "restricted":
+            if not target.is_possible():
+                return None
+
             self.vertex.drones.remove(self)
             used_edge.att_n_drones()
+            self.destination = target
             self.path.pop()
             self.in_transit = True
-            self.destination = self.path[-1]
             self.transit_edge = used_edge
-            return f"{self.name}-{self.vertex.name}>{self.path[-1].name}"
+            return f"{self.name}-{self.vertex.name}>{target.name}"
+
+        return None
